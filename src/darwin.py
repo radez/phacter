8ba5c50aa5de7518cf57c22d5061a5a1a6bc5e89
Dan Radez <dradez@redhat.com>
2011-11-17 13:35:22 -0500

    turning the OSes into modules instead of classes

--- src/darwin.py
+++ src/darwin.py
@@ -1,8 +1,9 @@
-import os,netifaces
-class Darwin:
-    def ipaddress(self):
-        return netifaces.ifaddresses('en0')[2][0]['addr']
-    def fqdn(self):
-        return 'server02.example.com'
-    def processorcount(self):
-        return int(os.popen('sysctl -n hw.ncpu').read())
+import os
+import netifaces
+
+def ipaddress():
+    return netifaces.ifaddresses('en0')[2][0]['addr']
+def fqdn():
+    return 'server02.example.com'
+def processorcount():
+    return int(os.popen('sysctl -n hw.ncpu').read())
